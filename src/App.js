616870855383aaefec6840ce795e39be11430f41
Dawn Hughes <Dawnhughesnow@gmail.com>
2025-08-31 17:43:48 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Weather from "./Weather";
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="weather-app">
+      <header>
+        <h1>Weather App</h1>
+      </header>
+      <main>
+        <Weather />
+      </main>
+      <footer>
+        Built by{" "}
+        <a href="https://github.com/FedDawb" target="_blank" rel="noreferrer">
+          Dawn Hughes
+        </a>{" "}
+        - hosted{" "}
+        <a
+          href="https://app.netlify.com/sites/fedawbweatherapp/overview"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify
+        </a>{" "}
+        code hosted{" "}
+        <a
+          href="https://github.com/FedDawb/VanillaWeatherProjecthttps://github.com/FedDawb/my-new-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Github
+        </a>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Weather.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function Weather() {
+  const [city, setCity] = useState("");
+  const [loaded, setLoaded] = useState(false);
+  const [weather, setWeather] = useState({});
+
+  function displayWeather(response) {
+    setLoaded(true);
+    setWeather({
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      description: response.data.weather[0].description,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "3fd91e83d21c4db97b409a3e896f8db6";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayWeather);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <input type="search" placeholder="Enter a city" onChange={updateCity} />
+      <button type="submit"> Submit</button>
+    </form>
+  );
+
+  if (loaded) {
+    return (
+      <div>
+        {form}
+        <ul style={{ listStyleType: "none" }}>
+          <li>Temperature: {Math.round(weather.temperature)}°C</li>
+          <li>Description:{weather.description}</li>
+          <li>Humidity: {weather.humidity}%</li>
+          <li>Wind: {weather.wind}km/h</li>
+          <li>
+            <img src={weather.icon} alt={weather.description} />
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
+  }
+}
